0653a0af569edef16c87bfcdfda5e8c364f0706f
unknown07724 <trash1.junk99@gmail.com>
2025-04-25 15:36:06 -0500

    Create imgflip.js

---
+++ Scrapers/imgflip.js
@@ -0,0 +1,5 @@
+// Get the meme description from the img-desc class
+const memeDescription = document.querySelector('.img-desc')?.textContent || 'No description found';
+
+// Output the description to the console
+console.log(memeDescription);
